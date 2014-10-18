0d30b906ad51c17a9c556412edd502099004d53d
Jules <jules.walter@gmail.com>
2014-10-17 19:32:11 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigBrother::Application.config.secret_key_base = '35c1a1bd564549d9b77c4cd5abf250b897d44726a9a462f59176d6e192683303be8d53f61fedca4a66073d1d38f0923d6f9c2dde38b987970d780d3a359a97eb'
