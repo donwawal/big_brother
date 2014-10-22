6dcd1d9f7dbb0b32ca84a86342e7541e6f27aaed
Jules <jules.walter@gmail.com>
2014-10-21 22:32:00 -0700

    basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def hello
-    render text: "Bienvenue a Grand frere, grande soeur!"
-  end
-
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 BigBrother::Application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get 'about' => 'static_pages#about'
   get 'contact_us' => 'static_pages#contact_us'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
